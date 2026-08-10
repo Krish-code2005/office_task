9505512614ddc49e7c9eb53a898e0a0dc5416253
Krish-code2005 <sthakriz098@gmail.com>
2026-08-10 16:33:38 +0545

    chore: Clean code

--- lib/core/services/local_notification_service.dart
+++ lib/core/services/local_notification_service.dart
@@ -1,65 +1,3 @@
-// import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-// import 'package:injectable/injectable.dart';
-
-// @lazySingleton
-// class LocalNotificationService {
-//   final FlutterLocalNotificationsPlugin _plugin = FlutterLocalNotificationsPlugin();
-
-//   Future<void> init() async {
-//     const androidSettings = AndroidInitializationSettings('@mipmap/ic_launcher');
-//     const iosSettings = DarwinInitializationSettings(
-//       requestAlertPermission: true,
-//       requestBadgePermission: true,
-//       requestSoundPermission: true,
-//     );
-
-//     const initSettings = InitializationSettings(
-//       android: androidSettings,
-//       iOS: iosSettings,
-//     );
-
-//     await _plugin.initialize(initSettings);
-
-//     const androidChannel = AndroidNotificationChannel(
-//       'reminders_channel',
-//       'Reminders',
-//       description: 'Notifications for product reminders',
-//       importance: Importance.high,
-//     );
-
-//     await _plugin
-//         .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
-//         ?.createNotificationChannel(androidChannel);
-//   }
-
-
-//   Future<void> scheduleReminder({
-//   required String title,
-//   required String body,
-// }) async {
-//   const androidDetails = AndroidNotificationDetails(
-//     'reminders_channel',
-//     'Reminders',
-//     channelDescription: 'Notifications for product reminders',
-//     importance: Importance.high,
-//     priority: Priority.high,
-//   );
-
-//   const iosDetails = DarwinNotificationDetails();
-
-//   const details = NotificationDetails(android: androidDetails, iOS: iosDetails);
-
-//   await Future.delayed(const Duration(seconds: 10));
-
-//   await _plugin.show(
-//     DateTime.now().millisecondsSinceEpoch ~/ 1000,
-//     title,
-//     body,
-//     details,
-//   );
-// }
-// }
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:injectable/injectable.dart';
@@ -76,7 +14,6 @@ class LocalNotificationService {
     tz.initializeTimeZones();
     try {
       // Set local location explicitly if needed, or let it detect device local
-      final currentTimeZone = await DateTime.now().timeZoneName;
       // Alternatively, default to local if auto-detection fails
       tz.setLocalLocation(tz.local);
     } catch (e) {
